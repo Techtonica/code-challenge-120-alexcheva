c33c3dd2c0bd2b3ca9bca164a6c562c9c7ed5c80
Alexandra Lukinicheva <a.lukinicheva@gmail.com>
2021-02-18 11:29:15 -0800

    fix spec

--- spec/120-spec.js
+++ spec/120-spec.js
@@ -1,11 +1,9 @@
 const groupBy = require('../challenges/120.js');
-
 const pomeranians = [
   { name: 'Osito', weight_kg: 3.5 },
   { name: 'Maní', weight_kg: 3.9 },
   { name: 'Bella', weight_kg: 7.2 }
 ];
-
 const answer1 = {
   '3': [
         { name: 'Osito', weight_kg: 3.5 },
@@ -13,12 +11,11 @@ const answer1 = {
       ],
   '7': [{ name: 'Bella', weight_kg: 7.2 }]
 };
-
 describe("Challenge 120:", () => {
   it("groupBy(pomeranians, (p) => Math.floor(p.weight_kg)) should return 'csi miami'", () => {
-    expect(groupBy(pomeranians, (p) => Math.floor(p.weight_kg)).toEqual(answer1));
-  });
+    expect(groupBy(pomeranians, (p) => Math.floor(p.weight_kg))).toEqual(answer1);//HERE
+  }); 
   it("groupBy(['one', 'two', 'three'], (s) => s.length) should return { '3': ['one', 'two'], '5': ['three'] }", () => {
-    expect(groupBy(['one', 'two', 'three'], (s) => s.length).toEqual({ '3': ['one', 'two'], '5': ['three'] }));
+    expect(groupBy(['one', 'two', 'three'], (s) => s.length)).toEqual({ '3': ['one', 'two'], '5': ['three'] }); //AND HERE
   });
 });
