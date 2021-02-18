a6497a62f9484759eb6eb36fb4f984b341f6a6e0
Alexandra Lukinicheva <a.lukinicheva@gmail.com>
2021-02-18 11:10:03 -0800

    solve 120

--- challenges/120.js
+++ challenges/120.js
@@ -5,42 +5,47 @@
 // In the \_functional\_ style you define that logic using a function
 // and use it as an argument.
 //
-//
-//
-// ```
-//
-// // Let's say you want to group your pomeranians by weight class.
-// pomeranians = [
-//   { name: 'Osito', weight_kg: 3.5 },
-//   { name: 'Maní', weight_kg: 3.9 },
-//   { name: 'Bella', weight_kg: 7.2 }
-// ]
-// groupBy(pomeranians, (p) => Math.floor(p.weight_kg))\
-// /*
-//  {
-//   '3': [
-//         { name: 'Osito', weight_kg: 3.5 },
-//         { name: 'Maní', weight_kg: 3.9 }
-//       ],
-//   '7': [{ name: 'Bella', weight_kg: 7.2 }]
-//  }
-// */
-//
-//  // or string by their length
-// groupBy(['one', 'two', 'three'], (s) => s.length);
-// // => { '3': ['one', 'two'], '5': ['three'] }
-//
-// ```
-//
-//
-//
 // Implement groupBy. Use other existing functions on Array if you want.
 //
+// var groupBy = function(xs, key) {
+//   return xs.reduce(function(rv, x) {
+//     (rv[x[key]] = rv[x[key]] || []).push(x);
+//     return rv;
+//   }, {});
+// };
 
+// console.log(groupBy(['one', 'two', 'three'], 'length'));
+// Let's say you want to group your pomeranians by weight class.
+pomeranians = [
+  { name: 'Osito', weight_kg: 3.5 },
+  { name: 'Maní', weight_kg: 3.9 },
+  { name: 'Bella', weight_kg: 7.2 }
+]
+//logic = key 
 function groupBy(arr, logic){
+  
   return(
-    // replace this line with your code
+    arr.reduce((acc, val) => {
+      const key = logic(val);
+      acc[key] = acc[key] || []
+      acc[key].push(val);
+      return acc;
+    },{})
   );
 }
 
+console.log(groupBy(pomeranians, (p) => Math.floor(p.weight_kg)));
+/*
+ {
+  '3': [
+        { name: 'Osito', weight_kg: 3.5 },
+        { name: 'Maní', weight_kg: 3.9 }
+      ],
+  '7': [{ name: 'Bella', weight_kg: 7.2 }]
+ }
+*/
+
+ // or string by their length
+console.log(groupBy(['one', 'two', 'three'], (s) => s.length));
+// => { '3': ['one', 'two'], '5': ['three'] }
 module.exports = groupBy;
